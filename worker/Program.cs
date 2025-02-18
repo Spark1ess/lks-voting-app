fb6b95f919e12506444609fc327574fc973e92c4
Spark1ess <sacca.privatemail@gmail.com>
2025-02-18 10:48:14 +0800

    Update Program.cs

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                var pgsql = OpenDbConnection("Host=lks-rds.cmnie79wromm.us-east-1.rds.amazonaws.com;Username=admins;Password=LKSNCC2024;");
                 var redisConn = OpenRedisConnection("master.lks-redis.rvrz8p.use1.cache.amazonaws.com:6379");
                 var redis = redisConn.GetDatabase();
 
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                            pgsql = OpenDbConnection("Host=lks-rds.cmnie79wromm.us-east-1.rds.amazonaws.com;Username=admins;Password=LKSNCC2024;");
                         }
                         else
                         { // Normal +1 vote requested
